ed29e61a8cf92034cb7b4a871fb18ed60f9d8b34
QSmally <github@qbot.eu>
2023-12-10 13:46:03 +0100

    Guide: finalise assembly language/linker

--- snippets/example.s
+++ snippets/example.s
@@ -1,35 +1,42 @@
 
 // QCPU 2 assembly syntax snippet
 
+; this define would be from @include "kernel/sysc.s"
+; a kext may also be from @include "kernel/kernel.s"
 @define fopen 0x05
+@define mmap 0x07
 
 @section globals ; shared by threads, max. 256 bytes, fixed size
 
-.path:      ascii "/etc/file" 0x00  ; ascii is just u8 with explicit encoding
-.pathl:     u8 .pathl - .path       ; expressions
+.path:      ascii "/etc/fstab" 0x00 ; ascii is just u8 with explicit encoding
+.pathsz:    u8 .pathsz - .path      ; expressions
 .pathptr:   u16 .path               ; path gets read as 16 bits
+
 .longaddr:  u24 0x123456
-.gobject:   u16 .global_object      ; reference to thread-local global object
+.objext:    u16 .larger_global_obj  ; reference to a larger object
 
-@section locals ; thread local, fixed size
+@section gextension ; extension on globals, fixed size
 
-.global_object:
+.larger_global_obj:
             ascii "hello world!" 0x00
 
 @section stack ; thread local, initially max. 256 bytes
 
                                     ; kernel firmware may have leading bytes
-.offset0:   reserve u8 24           ; reserve 24 bytes
-.offset1:   reserve u16 24          ; reserve 48 bytes
-.offset2:   u32 0x1234 0x5678       ; initialise 4 bytes
-                                    ; base stack pointer location
+.localvar0: reserve u8 24           ; reserve 24 bytes
+.localvar1: reserve u16 24          ; reserve 48 bytes
+.localvar2: u32 0x1234 0x5678       ; initialise 4 bytes
+.offset:                            ; base stack pointer location
 
-@section text ; readonly
+@section text ; readonly, executable
 
 main:       imm  rx,    .path       ; 'main' must be a public label for linking
             imm  ry,    .path'u     ; upper byte syntax 'u, for u8 implicitly 'l
-            sysc @fopen             ; fopen returns u8 file descriptor in rx
-            msp  +1
-            ast  rx
-            mst  ssb,   .offset1 + 1
+            imm  rz,    0b00000000  ; fopen mask
+            sysc @fopen             ; fopen(pathl, pathh, msk) -> (,, fd)
+            imm  rx,    0x00        ; size low byte
+            imm  ry,    0x04        ; size high byte, 1024
+            sysc @mmap              ; mmap(sizel, sizeh, fd) -> (addrl, addrh)
+            msp  +2
+            mstw sfb,   .offset     ; store at recently allocated stack location
 .deadlock:  jmp  0,     .deadlock   ; execs should end with sysc, jmp or ret
